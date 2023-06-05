db5427cad8068860b290aa283d01bdc8bbcfd71a
Gerrit Alex <admin@gerritalex.de>
2023-06-05 16:15:13 +0200

    delete afflicted since its not a thing

--- plater/scripts/afflicted/onupdate.lua
+++
@@ -1,9 +0,0 @@
-function (_, unitId, unitFrame, _, scriptTable)
-    local color = scriptTable.determineDebuffColor(unitId)
-
-    if color then
-        Plater.SetNameplateScale(unitFrame, scriptTable.config.activeScale)
-        Plater.FlashNameplateBorder(unitFrame)
-        Plater.SetNameplateColor(unitFrame, color)
-    end
-end

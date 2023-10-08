1ad03a0f88f71cb31ccef5eff03dcc9af14371c5
Gerrit Alex <admin@gerritalex.de>
2023-10-09 01:32:37 +0200

    add aug group cd tracking

---
+++ weakauras/group-cds/init-action.lua
@@ -0,0 +1,8 @@
+--- @type table<number, number>
+aura_env.durationForSpell = {
+    [49206] = 25, -- summon gargoyle
+    [1122] = 30, -- summon infernal
+    [265187] = 15, -- summon demonic tyrant
+    [205180] = 20, -- summon darkglare
+    [360194] = 16, -- deathmark
+}

6eb4c2d5ae176c17ef79c8a063bd21117be7f80c
Gerrit Alex <admin@gerritalex.de>
2023-11-01 02:06:01 +0100

    anotehr vuhdo update

--- weakauras/aug-vuhdo-pt-helper/init-action.lua
+++ weakauras/aug-vuhdo-pt-helper/init-action.lua
@@ -5,6 +5,7 @@ aura_env.active = vuhdo ~= nil
 
 --- @type table<string, number>
 aura_env.nameToSpecIdMap = {}
+local lastPrivateTanks = nil
 
 aura_env.log = function(...)
     if aura_env.config.dev.log then
@@ -86,22 +87,31 @@ end
 --- @param event string
 --- @param names table<number, string>
 local function setupPrivateTanks(event, names)
+    local nextPrivateTanks = table.concat(names, ",")
+
+    if nextPrivateTanks == lastPrivateTanks then
+        aura_env.log("setup identical to previous. skipping")
+        return
+    end
+
+    lastPrivateTanks = nextPrivateTanks
+
     local eventExplanationMap = {
         ["PLAYER_ENTERING_WORLD"] = "login or reload",
         ["READY_CHECK"] = "ready check",
-        [aura_env.customEventName] = "manual override seen"
+        [aura_env.customEventName] = "manual override seen",
+        ["UNIT_SPEC_CHANGED"] = "spec update received",
+        ["OPTIONS"] = "WeakAuras open"
     }
 
     aura_env.log("clearing private tanks. reason: " .. (eventExplanationMap[event] or event))
     vuhdo("pt clear")
 
-    local nextPts = table.concat(names, ",")
-
-    if nextPts == "" then
+    if nextPrivateTanks == "" then
         return
     end
 
-    lastArguments = nextPts
+    lastArguments = nextPrivateTanks
 
     aura_env.log("Setting up private tanks:")
 
@@ -109,20 +119,20 @@ local function setupPrivateTanks(event, names)
         local specId = aura_env.nameToSpecIdMap[name]
         local str = name
         if specId then
-            local _, specName, _, icon, _, _, className = GetSpecializationInfoByID(specId)
+            local _, specName, _, icon, _, className = GetSpecializationInfoByID(specId)
             str = aura_env.formattedSpecIconWithName(specName, icon, className, name)
         end
 
         aura_env.log("--> " .. str)
     end
 
-    vuhdo("pt " .. nextPts)
+    vuhdo("pt " .. nextPrivateTanks)
 end
 
 --- @param tbl table<number, string>
---- @param players table<number, string>
 --- @param limit number|nil
-local function populateTable(tbl, players, limit)
+--- @param players table<number, string>
+local function populateTable(tbl, limit, players)
     for _, name in pairs(players) do
         if limit and #tbl == limit then
             return
@@ -132,39 +142,56 @@ local function populateTable(tbl, players, limit)
     end
 end
 
-local function isAugmentationEvoker(name)
-    return aura_env.nameToSpecIdMap[name] == 1473
-end
-
-local function isHealer(name)
-    local specId = aura_env.nameToSpecIdMap[name]
-    
-    if not specId then
-        return false
+--- @param seenPlayers table<string, string>
+local function cleanupOrphanedManualPlayers(seenPlayers)
+    -- clean up manual overrides to not carry them around when they leave group
+    for key, name in pairs(manuallyAddedPlayers) do
+        if seenPlayers[name] == nil then
+            manuallyAddedPlayers[key] = nil
+        end
     end
-
-    return specId == 105 or specId == 1468 or specId == 270 or specId == 65 or specId == 256 or specId == 257 or
-        specId == 264
 end
 
 --- @param event string
 local function doGroupUpdate(event)
+    local bannedSpecs = {
+        [1473] = true, -- augmentation evoker
+        -- healers
+        [105] = true,
+        [1468] = true,
+        [270] = true,
+        [65] = true,
+        [256] = true,
+        [257] = true,
+        [264] = true
+    }
+
+    --- @type table<number, string>
     local players = {}
+    --- @type table<string, string>
+    local unitNameToTokenMap = {}
 
     for unit in WA_IterateGroupMembers() do
         if not UnitIsUnit(unit, "player") then
             local name = UnitName(unit)
 
-            if not isHealer(name) and not isAugmentationEvoker(name) then
+            if name then
+                unitNameToTokenMap[name] = unit
+            end
+            local specId = aura_env.nameToSpecIdMap[name]
+
+            if not specId or bannedSpecs[specId] == nil then
                 players[#players + 1] = name
             end
         end
     end
 
-    populateTable(players, manuallyAddedPlayers)
-    populateTable(players, players)
+    local names = {}
 
-    setupPrivateTanks(event, players)
+    cleanupOrphanedManualPlayers(unitNameToTokenMap)
+    populateTable(names, nil, manuallyAddedPlayers)
+    populateTable(names, nil, players)
+    setupPrivateTanks(event, names)
 end
 
 local function doRaidUpdate(event)
@@ -234,6 +261,8 @@ local function doRaidUpdate(event)
         end
     end
 
+    cleanupOrphanedManualPlayers(unitNameToTokenMap)
+
     local playerInstanceId = select(4, UnitPosition("player"))
 
     for _, dataset in pairs(aura_env.config.raid.priority) do
@@ -255,20 +284,13 @@ local function doRaidUpdate(event)
         end
     end
 
-    -- clean up manual overrides to not carry them around when they leave group
-    for key, name in pairs(manuallyAddedPlayers) do
-        if unitNameToTokenMap[name] == nil then
-            manuallyAddedPlayers[key] = nil
-        end
-    end
-
     --- @type table<number, string>
     local players = {}
 
-    populateTable(players, manuallyAddedPlayers, limit)
-    populateTable(players, highPriority, limit)
-    populateTable(players, mediumPriority, limit)
-    populateTable(players, lowPriority, limit)
+    populateTable(players, limit, manuallyAddedPlayers)
+    populateTable(players, limit, highPriority)
+    populateTable(players, limit, mediumPriority)
+    populateTable(players, limit, lowPriority)
 
     setupPrivateTanks(event, players)
 end
--- weakauras/aug-vuhdo-pt-helper/t1.lua
+++ weakauras/aug-vuhdo-pt-helper/t1.lua
@@ -1,7 +1,7 @@
--- PLAYER_ENTERING_WORLD, XEPHUI_AUG_VUHDO_MANUAL_OVERRIDE_UPDATE, UNIT_SPEC_CHANGED_party1, UNIT_SPEC_CHANGED_party2, UNIT_SPEC_CHANGED_party3, UNIT_SPEC_CHANGED_party4, UNIT_SPEC_CHANGED_raid1, UNIT_SPEC_CHANGED_raid2, UNIT_SPEC_CHANGED_raid3, UNIT_SPEC_CHANGED_raid4, UNIT_SPEC_CHANGED_raid5, UNIT_SPEC_CHANGED_raid6, UNIT_SPEC_CHANGED_raid7, UNIT_SPEC_CHANGED_raid8, UNIT_SPEC_CHANGED_raid9, UNIT_SPEC_CHANGED_raid10, UNIT_SPEC_CHANGED_raid11, UNIT_SPEC_CHANGED_raid12, UNIT_SPEC_CHANGED_raid13, UNIT_SPEC_CHANGED_raid14, UNIT_SPEC_CHANGED_raid15, UNIT_SPEC_CHANGED_raid16, UNIT_SPEC_CHANGED_raid17, UNIT_SPEC_CHANGED_raid18, UNIT_SPEC_CHANGED_raid19, UNIT_SPEC_CHANGED_raid20, UNIT_SPEC_CHANGED_raid21, UNIT_SPEC_CHANGED_raid22, UNIT_SPEC_CHANGED_raid23, UNIT_SPEC_CHANGED_raid24, UNIT_SPEC_CHANGED_raid25, UNIT_SPEC_CHANGED_raid26, UNIT_SPEC_CHANGED_raid27, UNIT_SPEC_CHANGED_raid28, UNIT_SPEC_CHANGED_raid29, UNIT_SPEC_CHANGED_raid30, UNIT_SPEC_CHANGED_raid31, UNIT_SPEC_CHANGED_raid32, UNIT_SPEC_CHANGED_raid33, UNIT_SPEC_CHANGED_raid34, UNIT_SPEC_CHANGED_raid35, UNIT_SPEC_CHANGED_raid36, UNIT_SPEC_CHANGED_raid37, UNIT_SPEC_CHANGED_raid38, UNIT_SPEC_CHANGED_raid39, UNIT_SPEC_CHANGED_raid40
+-- STATUS, PLAYER_ENTERING_WORLD, XEPHUI_AUG_VUHDO_MANUAL_OVERRIDE_UPDATE, UNIT_SPEC_CHANGED_party1, UNIT_SPEC_CHANGED_party2, UNIT_SPEC_CHANGED_party3, UNIT_SPEC_CHANGED_party4, UNIT_SPEC_CHANGED_raid1, UNIT_SPEC_CHANGED_raid2, UNIT_SPEC_CHANGED_raid3, UNIT_SPEC_CHANGED_raid4, UNIT_SPEC_CHANGED_raid5, UNIT_SPEC_CHANGED_raid6, UNIT_SPEC_CHANGED_raid7, UNIT_SPEC_CHANGED_raid8, UNIT_SPEC_CHANGED_raid9, UNIT_SPEC_CHANGED_raid10, UNIT_SPEC_CHANGED_raid11, UNIT_SPEC_CHANGED_raid12, UNIT_SPEC_CHANGED_raid13, UNIT_SPEC_CHANGED_raid14, UNIT_SPEC_CHANGED_raid15, UNIT_SPEC_CHANGED_raid16, UNIT_SPEC_CHANGED_raid17, UNIT_SPEC_CHANGED_raid18, UNIT_SPEC_CHANGED_raid19, UNIT_SPEC_CHANGED_raid20, UNIT_SPEC_CHANGED_raid21, UNIT_SPEC_CHANGED_raid22, UNIT_SPEC_CHANGED_raid23, UNIT_SPEC_CHANGED_raid24, UNIT_SPEC_CHANGED_raid25, UNIT_SPEC_CHANGED_raid26, UNIT_SPEC_CHANGED_raid27, UNIT_SPEC_CHANGED_raid28, UNIT_SPEC_CHANGED_raid29, UNIT_SPEC_CHANGED_raid30, UNIT_SPEC_CHANGED_raid31, UNIT_SPEC_CHANGED_raid32, UNIT_SPEC_CHANGED_raid33, UNIT_SPEC_CHANGED_raid34, UNIT_SPEC_CHANGED_raid35, UNIT_SPEC_CHANGED_raid36, UNIT_SPEC_CHANGED_raid37, UNIT_SPEC_CHANGED_raid38, UNIT_SPEC_CHANGED_raid39, UNIT_SPEC_CHANGED_raid40
 --- @param event "PLAYER_ENTERING_WORLD" | "UNIT_SPEC_CHANGED" | "XEPHUI_AUG_VUHDO_MANUAL_OVERRIDE_UPDATE"
-function (event, ...)
-    if not aura_env.active then
+function (_, event, ...)
+    if not aura_env.active or not event then
         return false
     end
 
@@ -15,18 +15,42 @@ function (event, ...)
 
             if specId then
                 local _, specName, _, icon = GetSpecializationInfoByID(specId)
-                local className = select(2, UnitClass(unit))
+                local className = UnitClassBase(unit)
 
-                aura_env.log(aura_env.formattedSpecIconWithName(specName, icon, className, name))
+                aura_env.log("received spec for " .. aura_env.formattedSpecIconWithName(specName, icon, className, name))
             end
 
-            aura_env.setup(event)
+            aura_env.setup("UNIT_SPEC_CHANGED")
         end
 
         return false
     end
 
-    if event == "READY_CHECK" or event == "PLAYER_ENTERING_WORLD" then
+    if event == "STATUS" then
+        for unit in WA_IterateGroupMembers() do
+            if not UnitIsUnit(unit, "player") then
+                local specId = WeakAuras.SpecForUnit(unit)
+                local name = UnitName(unit)
+
+                if name then
+                    aura_env.nameToSpecIdMap[name] = specId
+
+                    if specId then
+                        local _, specName, _, icon = GetSpecializationInfoByID(specId)
+                        local className = UnitClassBase(unit)
+
+                        aura_env.log(aura_env.formattedSpecIconWithName(specName, icon, className, name))
+                    end
+                end
+            end
+        end
+
+        aura_env.setup("UNIT_SPEC_CHANGED")
+
+        return false
+    end
+
+    if event == "READY_CHECK" or event == "PLAYER_ENTERING_WORLD" or event == aura_env.customEventName then
         if event == aura_env.customEventName then
             local id = ...
 
@@ -40,4 +64,3 @@ function (event, ...)
 
     return false
 end
-

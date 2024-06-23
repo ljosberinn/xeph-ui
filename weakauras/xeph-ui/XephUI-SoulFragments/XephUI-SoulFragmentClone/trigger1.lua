7d192f668fdf782c124e28ee2af1aeb2096c1a9b
Gerrit Alex <admin@gerritalex.de>
2024-06-24 00:03:52 +0200

    rewrite soul fragments

--- weakauras/xeph-ui/XephUI-SoulFragments/XephUI-SoulFragmentClone/trigger1.lua
+++ weakauras/xeph-ui/XephUI-SoulFragments/XephUI-SoulFragmentClone/trigger1.lua
@@ -1,54 +1,93 @@
--- UNIT_AURA:player,TRIGGER:2
---- @class SoulFragmentCloneState
---- @field show boolean
---- @field total 5
---- @field value number
---- @field index number
---- @field progressType "static"
---- @field changed boolean
+---UNIT_AURA:player
 
---- @param allstates table<number, SoulFragmentCloneState>
---- @param event "UNIT_AURA" | "TRIGGER" | "OPTIONS" | "STATUS"
---- @param updatedTriggerNumber nil | number | string
---- @param updatedTriggerStates table
---- @return boolean
-function (allstates, event, updatedTriggerNumber, updatedTriggerStates)
-	local stacks = aura_env.getStacks()
-	aura_env.currentStacks = stacks
+---@class SoulFragmentCloneState
+---@field show boolean
+---@field total 1
+---@field value number
+---@field index number
+---@field progressType "static"
+---@field changed boolean
+---@field isInMeta boolean
+---@field stacks number
 
-	local shouldPerformFullUpdate, canBail =
-		aura_env.shouldPerformFullUpdate(event, updatedTriggerNumber, updatedTriggerStates)
+---@param states table<number, SoulFragmentCloneState>
+---@param event "UNIT_AURA" | "STATUS" | "OPTIONS"
+function f(states, event, ...)
+	if event == "STATUS" then
+		local fragmentsInfo = C_UnitAuras.GetPlayerAuraBySpellID(aura_env.soulFragmentsBuffId)
+		local currentFragments = fragmentsInfo and fragmentsInfo.applications or 0
 
-	if canBail then
-		return false
-	end
+		local metamorphosisInfo = C_UnitAuras.GetPlayerAuraBySpellID(aura_env.metamorphosisBuffId)
+		local isInMeta = metamorphosisInfo ~= nil
 
-	local hasChanges = shouldPerformFullUpdate
-
-	for i = 1, 5, 1 do
-		local value = stacks >= i and 1 or 0
-
-		if allstates[i] then
-			local changed = shouldPerformFullUpdate or allstates[i].value ~= value
-			hasChanges = true
-
-			if changed then
-				allstates[i].value = value
-				allstates[i].changed = true
-			end
-		else
-			allstates[i] = {
+		for i = 1, 5 do
+			states[i] = {
 				show = true,
-				index = i,
-				progressType = "static",
-				value = value,
-				total = 5,
 				changed = true,
+				value = currentFragments >= i and 1 or 0,
+				total = 1,
+				progressType = "static",
+				index = i,
+				isInMeta = isInMeta,
+				stacks = currentFragments,
 			}
 		end
+
+		return true
 	end
 
-	aura_env.lastStacks = stacks
+	if event == "UNIT_AURA" then
+		---@type string, UnitAuraUpdateInfo?
+		local _, updateInfo = ...
 
-	return hasChanges or event == "STATUS"
+		if not updateInfo then
+			return false
+		end
+
+		-- taken from https://github.com/Gethe/wow-ui-source/blob/88f272e94c202ecbe11fa44945f6035c65c15176/Interface/AddOns/Blizzard_UnitFrame/EvokerEbonMightBar.lua#L51-L55
+		local isUpdatePopulated = updateInfo.isFullUpdate
+			or (updateInfo.addedAuras ~= nil and #updateInfo.addedAuras > 0)
+			or (updateInfo.removedAuraInstanceIDs ~= nil and #updateInfo.removedAuraInstanceIDs > 0)
+			or (updateInfo.updatedAuraInstanceIDs ~= nil and #updateInfo.updatedAuraInstanceIDs > 0)
+
+		if not isUpdatePopulated then
+			return false
+		end
+
+		local fragmentsInfo = C_UnitAuras.GetPlayerAuraBySpellID(aura_env.soulFragmentsBuffId)
+		local currentFragments = fragmentsInfo and fragmentsInfo.applications or 0
+
+		local metamorphosisInfo = C_UnitAuras.GetPlayerAuraBySpellID(aura_env.metamorphosisBuffId)
+		local isInMeta = metamorphosisInfo ~= nil
+
+		if states[1].isInMeta == isInMeta and states[1].stacks == currentFragments then
+			return false
+		end
+
+		local hasChanges = false
+
+		for i = 1, 5 do
+			local state = states[i]
+
+			if state.isInMeta ~= isInMeta then
+				state.isInMeta = isInMeta
+			end
+
+			if state.stacks ~= currentFragments then
+				state.stacks = currentFragments
+				state.changed = true
+				hasChanges = true
+			end
+
+			local value = currentFragments >= i and 1 or 0
+
+			if value ~= state.value then
+				state.value = value
+				state.changed = true
+				hasChanges = true
+			end
+		end
+
+		return hasChanges
+	end
 end
--- weakauras/xeph-ui/XephUI-SoulFragments/XephUI-SoulFragmentClone/trigger3.lua
+++
@@ -1,4 +0,0 @@
--- PLAYER_SPECIALIZATION_CHANGED:player, TRAIT_CONFIG_UPDATED
-function ()
-	aura_env.talentedSpiritBomb()
-end
